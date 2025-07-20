5d5989c3222bdb16fac57bce5dfdc251252e1b7e
Salvatore <71639887+LoMonacoSalvatore@users.noreply.github.com>
2025-07-20 20:03:28 +0200

    feat: api users

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,6 +1,6 @@
-import type { User } from '../types'
+import type { User } from '@/types'
 
-export async function getUsers(): Promise<User[]> {
+export async function fetchUsers(): Promise<User[]> {
   const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
   if (!response.ok) {
@@ -9,4 +9,15 @@ export async function getUsers(): Promise<User[]> {
 
   const users = await response.json()
   return users
+}
+
+export async function fetchUserById(id: string): Promise<User> {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users?id=${id}`)
+
+  if (!response.ok) {
+    throw new Error(`Error fetching user: ${response.status} ${response.statusText}`)
+  }
+
+  const user = await response.json()
+  return user
 }
